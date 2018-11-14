78f250a5db9040922728e7bfd8137140042d9445
Hoang-Kevin <44492522+Hoang-Kevin@users.noreply.github.com>
2018-11-14 16:18:45 +0100

    Add files via upload

--- Module 4/donnees.c
+++ Module 4/donnees.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <stdbool.h>
 
 int ft_lecturecriture()
 {
@@ -47,90 +47,4 @@ int ft_lecturecriture()
     return 0;
 }
 
-void swap(int *nb1, int *nb2)
-{
-    int temp; 
-    temp = *nb1;
-    *nb1 = *nb2;
-    *nb2 = temp;
-}
 
-int ft_max(int tableau[], int taille)
-{
-    int i;
-    int n = taille;
-    for(i=0; i != taille; i++)
-    {
-        if (tableau[i] > tableau[n])
-            n = i;
-    }
-    return n;
-}
-
-int ft_min(int tableau[], int taille)
-{
-    int i;
-    int n = taille;
-    for(i=0; i != taille; i++)
-    {
-        if(tableau[i] < tableau[n])
-            n = i;
-    }
-    return n;
-}
-
-void tri_selection(int tableau[], int tableau2[], int taille, int ordre)
-{
-    int res;
-    int taillebis = taille - 1;
-    int i;
-    switch(ordre)
-    {
-        case 1:
-        for(i=0; i < taille; i++)
-        {
-            res = ft_max(tableau, taillebis);
-            swap(&tableau[res], &tableau[taillebis]);
-            swap(&tableau2[res], &tableau2[taillebis]);
-            taillebis--;
-        }
-        break;
-
-        case 2: 
-        for(i=0; i < taille; i++)
-        {
-            res = ft_min(tableau, taillebis);
-            swap(&tableau[res], &tableau[taillebis]);
-            swap(&tableau2[res], &tableau2[taillebis]);
-            taillebis--;
-        }
-        break;
-        
-        case 3: 
-        for(i=0; i < taille; i++)
-        {
-            res = ft_min(tableau2, taillebis);
-            swap(&tableau2[res], &tableau2[taillebis]);
-            swap(&tableau[res], &tableau[taillebis]);
-            taillebis--;
-        }
-
-    }
-}
-
-int main()
-{
-    int tableau[10] = {1,2,4,6,8,10,12,14,16,18};
-    int tableau2[10] = {10,20,30,40,50,60,70,80,90,100};
-    int taille = 10;
-    int menu;
-    printf("choisissez\n");
-    scanf("%d", &menu);
-    tri_selection(tableau, tableau2, taille, menu);
-    for (int i = 0; i < taille; i++)
-        printf("%d,%d\n", tableau[i], tableau2[i]);
-
-    return 0;
-
-
-}
